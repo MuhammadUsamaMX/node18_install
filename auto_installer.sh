5b155b74ac4b4a793f15896f033cd5e4db2f897e
MuhammaD Usama <55662931+MuhammadUsamaMX@users.noreply.github.com>
2023-10-23 06:46:09 +0500

    Update auto_installer.sh

--- auto_installer.sh
+++ auto_installer.sh
@@ -374,13 +374,13 @@ select option in "${options[@]}"; do
         install_dependencies  # Install dependencies
         log "Dependencies installed."
         log "Starting Shopware installation."
-        install_shopware      # Install shopware6
+        #install_shopware      # Install shopware6
         log "Shopware installation completed."
         log "Starting RainLoop Webmail installation."
         install_rainloop      # Install webmail_rainloop
         log "RainLoop Webmail installation completed."
         log "Starting cloudflare setup."
-        cloudflare_setup      # setup cloudflare
+        #cloudflare_setup      # setup cloudflare
         log "Cloudflare setup Completed"
         exit
         ;;
